558d4648f04f962b09d0ab2f114f7e33af02f9aa
Mendivenson <mbarraganz@unal.edu.co>
2026-08-09 16:47:07 -0500

    retrive demand and exits as function

---
+++ code/funct/station_stress.R
@@ -0,0 +1,48 @@
+stop_stress <- function(dates = seq(as.Date('01-01-2026', format='%d-%m-%Y'),
+                                    as.Date('31-01-2026', format='%d-%m-%Y')),
+                        format='%d-%m-%Y'){
+
+  # Esta función resume la información de las salidas y entradas por estación del sistema troncal
+  # de TM con frecuencia horaria para un rango de días dado.
+  dates <- as.Date(dates, format=format)
+
+  if (any(dates < as.Date('2024-03-01'))) {
+    stop('NO HAY DATOS DE FECHAS ANTERIORES AL 1RO DE MARZO DE 2024.')
+  }
+
+  # nombres archivos zip
+  dates           <- format(dates, format='%Y%m%d')
+  validation.file <- paste0('validacionTroncal', dates, '.zip')
+  exit.file       <-  paste0('salidas', dates, '.zip')
+
+  # tablas de archivos x url
+  exit.table       <- read.csv('https://raw.githubusercontent.com/Mendivenson/datajam3/refs/heads/main/data/download%20url%20salidas.csv')
+
+# urls de descarga solicitadas
+  exit.url       <- exit.table[exit.table$nombre_archivo %in% exit.file, "url_descarga"]
+  validation.url <- validation.table[validation.table$nombre_archivo %in% validation.file, "url_descarga"]
+
+  # Bucle de descarga
+  exit <- pbapply::pblapply(exit.url,
+                 FUN = function(x){
+                   x = read.csv(archive::archive_read(x, format = 'zip'))
+                   x$Tiempo = x$Tiempo |> substr(start = 1, stop = 2) |> as.numeric()
+                   x$Estacion = regmatches(x$Estacion, gregexpr("(?<=\\()[^()]*(?=\\))", x$Estacion, perl = TRUE)) |>
+                     unlist() |>
+                     as.numeric() |>
+                     as.character()
+
+                   x |>
+                     dplyr::select('date' = Fecha_Transaccion,
+                                   'hour' = Tiempo,
+                                   'id station' = Estacion,
+                                   'validations' = Entradas_E,
+                                   'exits' = Salidas_S) |>
+                     group_by(date, hour, `id station`) |>
+                     dplyr::summarise(validations = sum(validations),
+                                      exits = sum(exits), .groups = 'drop')
+                 }) |>
+    do.call(what = rbind)
+
+  return(exit)
+}
